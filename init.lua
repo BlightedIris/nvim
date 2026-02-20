81feb79c0a3ab4b74580338fec02518d01640427
Ricardo Carvalho <ricardo.carvalho@carallon.com>
2026-02-20 17:23:08 +0000

    more stuff

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
 require('ricardo')
+
 -- plugins
 require('lualine').setup()
+require('bufferline').setup()
+require('plenary')
+require("todo-comments").setup()
+-- require("frizbee")
+require("blink-cmp").setup({ keymap = {
+    ['<C-Space>'] = { 'show', 'show_documentation' },
+    ['<CR>']      = { 'accept', 'fallback' },
+    ['<Tab>']     = { 'select_next', 'snippet_forward', 'fallback' },
+    ['<S-Tab>']   = { 'select_prev', 'snippet_backward', 'fallback' },
+    ['<C-e>']     = { 'hide' },
+    ['<C-n>']     = { 'select_next', 'fallback' },
+    ['<C-p>']     = { 'select_prev', 'fallback' },
+  },
+  sources = { default = { 'lsp', 'path', 'buffer' } },
+  completion = { ghost_text = { enabled = true } },
+})
+vim.lsp.enable('basedpyright')
+
+-- TODO: Use lush to create a theme
+-- TODO: blink-cmp 
---
+++ p.py
@@ -0,0 +1,8 @@
+def func(arg: str) -> None:
+    print("hello")
+
+def my_func():
+    func("hello")
+
+if __name__ == "__main__":
+    func("hello")
