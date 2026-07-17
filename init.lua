48e27516a32ab8aed8636d7d3438ecbe4297fff6
Ricardo Carvalho <ricardo.carvalho@carallon.com>
2026-07-17 18:15:12 +0100

    added theme command

--- init.lua
+++ init.lua
@@ -10,6 +10,8 @@ vim.diagnostic.config({
   virtual_text = false
 })
 
+vim.cmd.colorscheme('melange')
+
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
