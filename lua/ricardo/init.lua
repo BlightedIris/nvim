e784a45c95f6feef0bed835414e0fdaa5b0c5a2d
Ricardo Carvalho <ricardo.carvalho@carallon.com>
2026-07-31 07:55:09 +0100

    Forgot to save the other lua file

--- lua/ricardo/init.lua
+++ lua/ricardo/init.lua
@@ -1,7 +1,4 @@
 require("ricardo.gui_setup")
 
 -- Terminal:
-local shell = vim.fn.has("win32") == 1 and "powershell" or vim.o.shell
-print("Shell set to:")
-print(shell)
-vim.keymap.set("n", "<leader>t", function() vim.cmd.terminal(shell) end)
+
