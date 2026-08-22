c798f85cb396e3fdf67605e9b9f8023cbb0aac9f
BightedIris <blightediris@gmail.com>
2026-08-22 17:23:21 +0100

    Added default tabs and stuff

--- init.lua
+++ init.lua
@@ -14,3 +14,12 @@ vim.cmd.colorscheme('kanagawa-dragon')
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+-- global default: tabs
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+
+-- .editorconfig overrides per-project (already the 0.11 default; set explicitly if you want it pinned)
+vim.g.editorconfig = true
